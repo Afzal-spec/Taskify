93b2fb5e919c810f3f33acfdcb2db843d708d49a
Afzal Mohammed <shaikmominmohammedafzal@gmail.com>
2025-12-10 19:00:22 +0530

    Add project files.

---
+++ Taskify.Api/Models/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Taskify.Api.Models
+{
+    public class TaskItem
+    {  
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime? DueDate { get; set; }
+        public string Priority { get; set; } = "Medium";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
