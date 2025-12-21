8ac0cedac22b8505e40cf5972d827100fc7ee18a
Afzal Mohammed <shaikmominmohammedafzal@gmail.com>
2025-12-21 19:33:26 +0530

    Journal Modal creation

---
+++ Taskify.Api/Models/JournalEntry.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskify.Api.Models
+{
+    public class JournalEntry
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        [MinLength(10)]
+        public string Content { get; set; }
+        public int Mood { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
