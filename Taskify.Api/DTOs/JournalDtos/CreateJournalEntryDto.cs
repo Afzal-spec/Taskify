ab2522018da0eb278ed8680bb3bda8558364eee8
Afzal Mohammed <shaikmominmohammedafzal@gmail.com>
2025-12-24 11:26:37 +0530

    journal changes

---
+++ Taskify.Api/DTOs/JournalDtos/CreateJournalEntryDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskify.Api.DTOs.JournalDtos
+{
+    public class CreateJournalEntryDto
+    {
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        public string Content { get; set; }
+
+        public int Mood { get; set; } // 1–5
+    }
+}
---
+++ Taskify.Api/DTOs/JournalDtos/JournalEntryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Taskify.Api.DTOs.JournalDtos
+{
+    public class JournalEntryDto
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+        public int Mood { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
