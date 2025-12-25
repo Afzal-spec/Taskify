72b4017f101e4b0283c9471c97e071d57b7d2ad7
Afzal Mohammed <shaikmominmohammedafzal@gmail.com>
2025-12-25 14:40:41 +0530

    changes for habits

--- Taskify.Api/Data/AppDbContext.cs
+++ Taskify.Api/Data/AppDbContext.cs
@@ -12,6 +12,11 @@ namespace Taskify.Api.Data
         public DbSet<TaskItem> Tasks { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Note> Notes { get; set; }
+        public DbSet<JournalEntry> JournalEntries { get; set; }
+        public DbSet<Habit> Habits { get; set; }
+        public DbSet<HabitLog> HabitLogs { get; set; }
+
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -33,6 +38,25 @@ namespace Taskify.Api.Data
                 .HasOne(n => n.User)
                 .WithMany(u => u.Notes)
                 .HasForeignKey(n => n.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<JournalEntry>()
+                .HasQueryFilter(j => !j.IsDeleted);
+
+            modelBuilder.Entity<JournalEntry>()
+                .HasIndex(j => new { j.UserId, j.Date })
+                .IsUnique();
+
+            modelBuilder.Entity<Habit>()
+                .HasOne(h => h.User)
+                .WithMany(u => u.Habits)
+                .HasForeignKey(h => h.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<HabitLog>()
+                .HasOne(hl => hl.Habit)
+                .WithMany(h => h.Logs)
+                .HasForeignKey(hl => hl.HabitId)
                 .OnDelete(DeleteBehavior.Cascade);
 
             base.OnModelCreating(modelBuilder);
