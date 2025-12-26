67f2685e9fd4eab63cd066f52cc0c5f91e38c87f
Afzal Mohammed <shaikmominmohammedafzal@gmail.com>
2025-12-26 12:25:33 +0530

    HabitLog Repository file

---
+++ Taskify.Api/Repositories/HabitLogRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Taskify.Api.Data;
+using Taskify.Api.Models;
+
+namespace Taskify.Api.Repositories
+{
+    public class HabitLogRepository : IHabitLogRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public HabitLogRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<HabitLog> AddAsync(HabitLog log)
+        {
+            dbContext.HabitLogs.Add(log);
+            await dbContext.SaveChangesAsync();
+            return log;
+        }
+
+        public async Task<HabitLog?> GetByDateAsync(Guid habitId, DateTime date, int userId)
+        {
+            return await dbContext.HabitLogs
+                .Include(l => l.Habit)
+                .FirstOrDefaultAsync(l =>
+                    l.HabitId == habitId &&
+                    l.Habit.UserId == userId &&
+                    l.Date.Date == date.Date
+                    );
+        }
+
+        public async Task<List<HabitLog>> GetByHabitAsync(Guid habitId, int userId)
+        {
+            return await dbContext.HabitLogs
+                .Include(l => l.Habit)
+                .Where(l =>
+                    l.HabitId == habitId &&
+                    l.Habit.UserId == userId
+                )
+                .OrderByDescending(l => l.Date)
+                .ToListAsync();
+        }
+        public async Task UpdateAsync(HabitLog log, int userId)
+        {
+            await dbContext.SaveChangesAsync();
+        }
+        public async Task<List<HabitLog>> GetByDateAsync(DateTime date, int userId)
+        {
+            return await dbContext.HabitLogs
+                .Include(l => l.Habit)
+                .Where(l =>
+                    l.Habit.UserId == userId &&
+                    l.Date.Date == date.Date
+                )
+                .ToListAsync();
+        }
+
+    }
+}
---
+++ Taskify.Api/Repositories/IHabitLogRepository.cs
@@ -0,0 +1,14 @@
+﻿using Taskify.Api.Models;
+
+namespace Taskify.Api.Repositories
+{
+    public interface IHabitLogRepository
+    {
+        Task<HabitLog?> GetByDateAsync(Guid habitId, DateTime date, int userId);
+        Task<HabitLog> AddAsync(HabitLog log);
+        Task<List<HabitLog>> GetByHabitAsync(Guid habitId, int userId);
+        Task UpdateAsync(HabitLog log, int userId);
+        Task<List<HabitLog>> GetByDateAsync(DateTime date, int userId);
+
+    }
+}
