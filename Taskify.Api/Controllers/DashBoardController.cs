f4cba2aa2c24ca354b3812115d3f415861606377
Afzal Mohammed <shaikmominmohammedafzal@gmail.com>
2025-12-29 15:32:26 +0530

    Implementing Habit Dashboard

---
+++ Taskify.Api/Controllers/DashBoardController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Taskify.Api.Services;
+
+namespace Taskify.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class DashBoardController : ControllerBase
+    {
+        private readonly DashBoardService dashBoardService;
+
+        public DashBoardController(DashBoardService dashBoardService)
+        {
+            this.dashBoardService = dashBoardService;
+        }
+        [HttpGet("today")]
+        public async Task<IActionResult> GetTodayStatus()
+        {
+            return Ok(await dashBoardService.GetTodayStatusAsync());
+        }
+
+        [HttpGet("habits/streak-summary")]
+        public async Task<IActionResult> GetHabitStreakSummary()
+        {
+            return Ok(await dashBoardService.GetHabitStreakSummaryAsync());
+        }
+
+
+    }
+}
